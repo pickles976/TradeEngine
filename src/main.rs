bda1b7e4e83e2cac1ef3dc65bb9e832eb4aa6688
unknown <sebaslogo@gmail.com>
2023-04-11 15:41:18 -0500

    Added ability to do transactions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 #[allow(non_snake_case)]
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum OrderKind {
     BUY,
     SELL,
@@ -25,20 +25,124 @@ impl Order {
     }
 }
 
-fn main() {
-    
-    let mut market: HashMap<String, Vec<Order>> = HashMap::new();
+struct OrderRequest {
+    item: String,
+    order: Order,
+}
 
-    let order = Order::new(OrderKind::BUY, 100, 2.0);
-    let item = "corn".to_uppercase();
+impl OrderRequest {
+    fn new(item: String, kind: OrderKind, amount: u32, price_per: f32) -> OrderRequest {
+        OrderRequest { 
+            item: item.to_uppercase(), 
+            order: Order::new(kind, amount, price_per), 
+        }
+    }
+}
 
-    if !market.contains_key(&item) {
-        market.insert(item, vec![order]);
-    } else {
-        let orders = market.get_mut(&item).unwrap();
-        orders.push(order);      
+struct Market {
+    map: Box<HashMap<String, Vec<Order>>>,
+}
+
+impl Market {
+    fn new() -> Market {
+        Market {
+            map: Box::new(HashMap::new()),
+        }
     }
 
-    println!("{:?}", market);
+    pub fn place_order(&mut self, order_request: OrderRequest) {
 
+        let item = order_request.item;
+        let order = order_request.order;
+
+        if !self.map.contains_key(&item) {
+            self.map.insert(item, vec![order]);
+        } else {
+            let orders: &mut Vec<Order> = self.map.get_mut(&item).unwrap();
+
+            // transact
+            transact(order, orders);
+        }
+
+        println!("{:?}", self.map);
+
+    } 
+
+}
+
+fn transact(order: Order, orders: &mut Vec<Order>){
+
+    match order.kind {
+        OrderKind::BUY => buy(order, orders),
+        OrderKind::SELL => sell(order, orders),
+    };
+
+}  
+
+fn buy(order: Order, orders: &mut Vec<Order>){
+
+    let temp_orders: &mut Vec<Order> = orders;
+    let mut order = order;
+
+    for i in 0..temp_orders.len() {
+        if temp_orders[i].kind == OrderKind::SELL {
+            if temp_orders[i].price_per <= order.price_per {
+                if temp_orders[i].amount > order.amount {
+                    temp_orders[i].amount -= order.amount;
+                    order.amount = 0;
+                } else if temp_orders[i].amount < order.amount {
+                    order.amount -= temp_orders[i].amount;
+                    temp_orders[i].amount = 0;
+                } else {
+                    temp_orders[i].amount = 0;
+                    order.amount = 0;
+                }
+            }
+        }
+    }
+
+    temp_orders.push(order);
+    temp_orders.retain(|x| x.amount > 0);
+
+}
+
+fn sell(order: Order, orders: &mut Vec<Order>){
+
+    let temp_orders: &mut Vec<Order> = orders;
+    let mut order = order;
+
+    for i in 0..temp_orders.len() {
+        if temp_orders[i].kind == OrderKind::BUY {
+            if temp_orders[i].price_per >= order.price_per {
+                if temp_orders[i].amount > order.amount {
+                    temp_orders[i].amount -= order.amount;
+                    order.amount = 0;
+                } else if temp_orders[i].amount < order.amount {
+                    order.amount -= temp_orders[i].amount;
+                    temp_orders[i].amount = 0;
+                } else {
+                    temp_orders[i].amount = 0;
+                    order.amount = 0;
+                }
+            }
+        }
+    }
+
+    temp_orders.push(order);
+    temp_orders.retain(|x| x.amount > 0);
+
+}
+
+fn main() {
+    
+    let mut market = Market::new();
+
+    let order1 = OrderRequest::new("corn".to_string(), OrderKind::SELL, 100, 2.1);
+    let order2 = OrderRequest::new("corn".to_string(), OrderKind::SELL, 50, 2.5);
+    let order3 = OrderRequest::new("corn".to_string(), OrderKind::BUY, 115, 2.6);
+
+    market.place_order(order1);
+    market.place_order(order2);
+    market.place_order(order3);
+    
 }
